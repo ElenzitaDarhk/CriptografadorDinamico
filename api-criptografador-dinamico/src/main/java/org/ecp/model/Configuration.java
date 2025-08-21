4169ccf0e3688d86a144617998c06ef39e34a4ee
Elen <elencp87@gmail.com>
2025-08-21 20:52:23 -0300

    Add: Java Rest API

---
+++ api-criptografador-dinamico/src/main/java/org/ecp/model/Configuration.java
@@ -0,0 +1,37 @@
+package org.ecp.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Configuration")
+public class Configuration extends PanacheEntity {
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "appClientId")
+	private AppClient appClient;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "algorithmId")
+	private Algorithm algorithm;
+    
+	@Column(name = "KeyWord" , nullable = false)
+    public String keyWord;
+	
+	@Column(name = "InitialDate" , nullable = false)
+    public LocalDateTime initialDate;
+	
+	@Column(name = "FinalDate", nullable = true)
+    public LocalDateTime finalDate;
+	
+	@Column(name = "Enable" , nullable = false)
+    public Boolean enable;
+}
